435160d0cbc35cf6432fde1433730054f9957a90
추다율 <128597263+dayul@users.noreply.github.com>
2024-02-02 10:53:28 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.c
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.c
@@ -3,14 +3,8 @@
 #include <stdlib.h>
 
 int solution(int n, int k) {
-    int answer = 0;
     
-    answer = (n * 12000) + (k * 2000);
-    
-    
-    if ((n / 10) >= 1) {
-        answer -= (n/10) * 2000; 
-    }
+    int answer = 12000 * n + 2000 * (k - n / 10);
     
     return answer;
 }
