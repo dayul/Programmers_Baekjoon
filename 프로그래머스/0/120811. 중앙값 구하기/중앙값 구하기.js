762e5067cb80b1aef47f9e10159af48ecf0efbd0
추다율 <128597263+dayul@users.noreply.github.com>
2024-04-27 13:58:19 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,10 +1,12 @@
 function solution(array) {
     var answer = 0;
     
-    array.sort((a, b) => {
+    // 오름차순 정렬
+    array.sort((a, b) => {      
         return a - b;
     })
     
+    // 배열 길이를 2로 나누고, 정수부분만 저장
     len = Math.trunc(array.length / 2);
     
     answer = array[len];
