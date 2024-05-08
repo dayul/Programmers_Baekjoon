3fcf521654313baed62f1e3597b13886c2aecce0
추다율 <128597263+dayul@users.noreply.github.com>
2024-05-08 15:59:25 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,10 +1,15 @@
 function solution(myString, pat) {
     var answer = 0;
     
-    // 두 문자 모두 대문자로 통일 한 후 indexOf로 검색
-    if(myString.toUpperCase().indexOf(pat.toUpperCase()) != -1) {
+    // 1. 두 문자 모두 대문자로 통일 한 후 indexOf로 검색
+    // if(myString.toUpperCase().indexOf(pat.toUpperCase()) != -1) {
+    //     answer = 1;
+    // } 
+    
+    // 2. includes() 사용
+    if(myString.toUpperCase().includes(pat.toUpperCase())) {
         answer = 1;
-    } 
+    }
     
     return answer;
 }
