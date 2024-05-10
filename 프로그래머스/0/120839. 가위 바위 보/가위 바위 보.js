aa5e1df9dff0881c669cab47f4c1c0bc48684afd
추다율 <128597263+dayul@users.noreply.github.com>
2024-05-10 17:35:00 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,12 +1,21 @@
 function solution(rsp) {
-    var answer = '';
+
+    // 1. forEach() 사용 - 전개구문 대신 split() 사용 가능
+    // var answer = '';
+    // [...rsp].forEach(num => {
+    //     if(num === '2')   answer += 0;
+    //     else if(num === '0')   answer += 5;
+    //     else if(num === '5')   answer += 2;
+    // })
     
-    // 전개구문 대신 split() 사용 가능
-    [...rsp].forEach(num => {
-        if(num === '2')   answer += 0;
-        else if(num === '0')   answer += 5;
-        else if(num === '5')   answer += 2;
-    })
+    // 객체 활용, map() 사용
+    let arr = {
+        2 : 0,
+        0 : 5,
+        5 : 2
+    }
+    
+    var answer = [...rsp].map(i => arr[i]).join('');
     
     return answer;
 }
