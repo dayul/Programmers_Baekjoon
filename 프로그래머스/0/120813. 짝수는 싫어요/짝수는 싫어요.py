b9f1ff9d23b6ef5c544b98046af622024947180e
추다율 <128597263+dayul@users.noreply.github.com>
2024-05-12 23:51:16 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -1,6 +1,13 @@
 def solution(n):
-    answer = []
-    for i in range(1, n+1, 2):
-        answer.append(i)
+    # 1.for문 사용, append()
+    # answer = []
+    # for i in range(1, n+1, 2):
+    #     answer.append(i)
+    
+    # 2. 한줄에 작성, +2 하는 방법
+    # answer = [i for i in range(1, n+1, 2)]
+    
+    # 3. 한줄에 작성, if문
+    answer = [i for i in range(n+1) if i%2]
     
     return answer
