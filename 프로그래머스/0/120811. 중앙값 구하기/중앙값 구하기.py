394128c93ccd5a298ffdd54b060efc79057a0173
추다율 <128597263+dayul@users.noreply.github.com>
2024-05-13 00:06:08 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -3,8 +3,12 @@ def solution(array):
     
     array.sort()
     
+    # 1.
     # len(array) / 2를 하면 소수점까지 나오므로
     # int()를 사용해서 정수로 변환
-    answer = array[int(len(array) / 2)]
+    # answer = array[int(len(array) / 2)]
+    
+    # 2. // 사용하기 (정수값을 보여줌)
+    answer = array[len(array) // 2]
 
     return answer
