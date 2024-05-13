8c5535816345fe316be7b7d1b7ff50772ac8d957
추다율 <128597263+dayul@users.noreply.github.com>
2024-05-13 20:15:54 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.py
@@ -1,8 +1,12 @@
 def solution(n, numlist):
     answer = []
     
-    for i in numlist:
-        if i % n == 0:
-            answer.append(i)
+    # 1.
+    # for i in numlist:
+    #     if i % n == 0:
+    #         answer.append(i)
+    
+    # 2. 한번에 쓰기
+    answer = [i for i in numlist if i % n == 0]
     
     return answer
