01dd6900d618bc7225223d1925844d195005dbd3
추다율 <128597263+dayul@users.noreply.github.com>
2024-05-15 00:04:17 +0900

    [level 0] Title: 5명씩, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.py
+++ 프로그래머스/0/181886. 5명씩/5명씩.py
@@ -1,5 +1,7 @@
 def solution(names):
     answer = []
-    for i in range(0, len(names), 5):
-        answer.append(names[i])
+    # for i in range(0, len(names), 5):
+    #     answer.append(names[i])
+    
+    answer = names[::5]
     return answer
