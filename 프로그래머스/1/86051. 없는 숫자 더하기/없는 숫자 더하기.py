296a2efcd29fa79cc9c060dc04c9f7c6165f8ed3
추다율 <128597263+dayul@users.noreply.github.com>
2024-06-06 18:16:02 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,8 +1,13 @@
 def solution(numbers):
-    answer = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # 1. 내가 푼 방법
+#     answer = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     
-    for i in numbers:
-        if i in answer : 
-            answer.remove(i)
+#     for i in numbers:
+#         if i in answer : 
+#             answer.remove(i)
+#     return sum(answer)
+
+    # 천재적인 방법
+    return 45 - sum(numbers)
     
-    return sum(answer)
+    
