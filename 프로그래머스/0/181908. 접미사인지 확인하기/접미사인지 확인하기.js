e98b53c81c380bf48a32bc6c6148d5fabbca72f9
추다율 <128597263+dayul@users.noreply.github.com>
2024-06-12 00:16:51 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -2,7 +2,7 @@ function solution(my_string, is_suffix) {
     var answer = 0;
     
     let len = my_string.length - is_suffix.length;
-    if(my_string.slice(len, my_string.length) === is_suffix) {
+    if(my_string.slice(len) === is_suffix) {
         answer = 1;
     }
     
