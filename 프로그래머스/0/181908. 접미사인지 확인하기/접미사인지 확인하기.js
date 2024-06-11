f6bbc94e6b6dc415d230341210f2521778f26c60
추다율 <128597263+dayul@users.noreply.github.com>
2024-06-12 00:15:58 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,11 @@
+function solution(my_string, is_suffix) {
+    var answer = 0;
+    
+    let len = my_string.length - is_suffix.length;
+    if(my_string.slice(len, my_string.length) === is_suffix) {
+        answer = 1;
+    }
+    
+    
+    return answer;
+}
