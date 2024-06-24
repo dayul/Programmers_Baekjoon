c78c3178b4c00cee3da587bd3141c8e52d75efeb
추다율 <128597263+dayul@users.noreply.github.com>
2024-06-25 02:35:21 +0900

    [level 0] Title: 세로 읽기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,11 +1,16 @@
 function solution(my_string, m, c) {
     var answer = '';
     
+    // 1.
     // c번째부터 시작하므로 index는 0부터 시작 => c - 1
     // i가 m씩 증가하면서 c번째 열 요소값이 됨
-    for(let i = c-1; i < my_string.length; i += m) {
-        answer += my_string[i];
-    }
+    // for(let i = c-1; i < my_string.length; i += m) {
+    //     answer += my_string[i];
+    // }
+    
+    // 2.
+    // filter는 배열에서 사용
+    answer = [...my_string].filter((a, i) => i % m == c - 1).join("");
     
     return answer;
 }
