296cafc796a2b792ee52cce1bdc63d3d50469dc7
추다율 <128597263+dayul@users.noreply.github.com>
2024-06-29 16:46:29 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,7 +1,9 @@
 function solution(n) {
     var answer = 0;
+    // 문자로 형변환
     n = n.toString();
     
+    // 전개 연산자로 배열로 변환 후 내림차순 sort
     answer = [...n].sort((a, b) => {
         a = Number(a);
         b = Number(b);
