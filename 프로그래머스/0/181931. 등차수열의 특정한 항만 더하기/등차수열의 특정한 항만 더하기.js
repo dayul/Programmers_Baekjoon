1485ad7ff36631c5cca6efed45c0430dff8f833a
추다율 <128597263+dayul@users.noreply.github.com>
2024-07-05 14:56:00 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,8 +1,14 @@
 function solution(a, d, included) {
     // reduce로 현재 값(cur)이 true이면 맞는 항을 계산에 누적
-    var answer = included.reduce((acc, cur, idx) => {
-       return cur ? acc + a + d * idx : acc; 
-    }, 0);
+    // var answer = included.reduce((acc, cur, idx) => {
+    //    return cur ? acc + a + d * idx : acc; 
+    // }, 0);
+    
+    // map()
+    var answer = 0;
+    included.map((v, idx) => {
+        if(v) answer += a + d * idx;
+    })
     
     return answer;
 }
