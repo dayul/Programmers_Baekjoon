0a178b4f67db3cc4e3d50165de2ef8ae4ad28ce1
추다율 <128597263+dayul@users.noreply.github.com>
2024-07-28 00:11:28 +0900

    [level 1] Title: 내적, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,8 +1,11 @@
 function solution(a, b) {
-    var answer = 1234567890;
     
-    // 1. reduce() : 누적 값 acc에 누적, 초깃갑 0으로 설정
-    answer = a.reduce(((acc, cur, index) => acc += a[index] * b[index]), 0);
+    // 1. reduce() : 누적 값 acc에 누적, 초깃값 0으로 설정
+    // answer = a.reduce(((acc, cur, index) => acc += a[index] * b[index]), 0);
+    
+    // 2. reduce(), map() 
+    // a[i]와 b[i]의 곱을 map()로 return하고 reduce()로 총합을 구함
+    let answer = a.map((cur, index) => cur * b[index]).reduce((a, b) => a + b);
     
     return answer;
 }
