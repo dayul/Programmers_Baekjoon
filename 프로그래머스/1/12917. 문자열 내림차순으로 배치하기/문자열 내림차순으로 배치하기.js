e36f2d2973f301cf482f092dbc0699813b1b247b
추다율 <128597263+dayul@users.noreply.github.com>
2024-07-29 23:59:56 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,5 +1,8 @@
 function solution(s) {
     // 문자열 내림차순 정렬하기
-    var answer = [...s].sort((a, b) => b > a ? 1 : -1).join("");
+    // var answer = [...s].sort((a, b) => b > a ? 1 : -1).join("");
+    
+    // reverse()
+    var answer =  s.split("").sort().reverse().join("");
     return answer;
 }
