db4ffb00896fda0b7095a694b89c620588858362
추다율 <128597263+dayul@users.noreply.github.com>
2024-07-30 21:24:02 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -7,7 +7,6 @@ function solution(price, money, count) {
     }
 
     // 금액이 부족하지 않을 경우 0을, 아니면 필요한 돈에서 가지고 있는 돈을 빼서 부족한 금액을 구함
-    // (부족한 금액이 음수여야 함)
-    answer = (answer - money <= 0) ? 0 : answer - money;
+    answer = (answer < money) ? 0 : answer - money;
     return answer;
 }
