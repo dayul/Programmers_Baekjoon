6013ee6dc98719f877716cee5dd138ed8bffe92a
추다율 <128597263+dayul@users.noreply.github.com>
2024-08-01 23:49:16 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+    let answer = [];
+    // 공백을 기준으로 문자열을 나눔
+    s = s.split(" ");
+    
+    // arrow function에 {}가 있다면 return을 써야함(보통 2줄 이상)
+    // 없다면 생략
+    for(let str of s)
+        answer.push(str.split("").map((a, i) =>  i % 2 == 0 ? a.toUpperCase() : a.toLowerCase(), " ").join(""));
+
+    answer = answer.join(" ");
+    
+    return answer;
+}
