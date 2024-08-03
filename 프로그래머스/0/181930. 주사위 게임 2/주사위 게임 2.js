a68f8155a839ac882d924d5c197d682cf82a0eb0
추다율 <128597263+dayul@users.noreply.github.com>
2024-08-03 21:47:20 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,8 +1,8 @@
 function solution(a, b, c) {
-    var answer = (a + b + c) * (a * a + b * b + c * c);
+    var answer = (a + b + c) * (a ** 2+ b ** 2 + c ** 2);
     
     if(a === b && a === c && b === c)
-        answer = (a + b + c) * (a * a + b * b + c * c) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));
+        answer = (a + b + c) * (a ** 2+ b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3);
     
     else if(a !== b && b !== c && a !== c) 
         answer = a + b + c;
