29d5b2b87dedc072899f322322a597ea27739603
추다율 <128597263+dayul@users.noreply.github.com>
2024-08-04 22:48:51 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 2.49 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,15 @@
+function solution(t, p) {
+    let answer = 0;
+    // 문자열을 추출할 수 있을만큼의 횟수를 계산 (+1을 해야 마지막까지 다 포함됨)
+    let len = t.length - p.length + 1;
+    
+    for(let i = 0; i < len; i++) {
+        // 문자열에서 한칸씩 앞으로 가며 문자열을 슬라이싱함
+        let num = parseInt(t.substring(i, p.length + i));
+        
+        if(num <= parseInt(p))
+            answer++;
+    }
+    
+    return answer;
+}
