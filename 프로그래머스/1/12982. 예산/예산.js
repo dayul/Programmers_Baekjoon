9e358a6574c5b2f63a0ae4f1c98a8f87c1c6827a
추다율 <128597263+dayul@users.noreply.github.com>
2024-08-06 00:45:17 +0900

    [level 1] Title: 예산, Time: 0.16 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -1,18 +1,25 @@
-function solution(d, budget) {
-    // 다 구매가능할 때, 먼저 초깃값 지정
-    var answer = d.length;
+// function solution(d, budget) {
+//     // 다 구매가능할 때, 먼저 초깃값 지정
+//     var answer = d.length;
     
-    // 적은 예산부터 사용
-    d.sort((a, b) => a - b);
+//     // 적은 예산부터 사용
+//     d.sort((a, b) => a - b);
 
-    for(let i = 0; i < d.length; i++) {
-        budget -= d[i];
+//     for(let i = 0; i < d.length; i++) {
+//         budget -= d[i];
         
-        if(budget < 0) {
-            answer = i;
-            break;
-        }
-    }
+//         if(budget < 0) {
+//             answer = i;
+//             break;
+//         }
+//     }
 
-    return answer;
+//     return answer;
+// }
+
+// 다른 풀이 : true - 1, false - 0 이용
+function solution(d, budget) {
+    return d.sort((a, b) => a - b)
+        .reduce((count, price) => count + ((budget -= price) >= 0), 0);
+    // 0보다 같거나 클 때만 해당
 }
