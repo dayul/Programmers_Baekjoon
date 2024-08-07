0de421a76b0369a95965f1de125938e5f761f219
추다율 <128597263+dayul@users.noreply.github.com>
2024-08-08 00:06:45 +0900

    [level 1] Title: 최소직사각형, Time: 3.75 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -2,12 +2,15 @@ function solution(sizes) {
     const width = [];
     const height = [];
 
-    for(let i = 0; i < sizes.length; i++) {
-        const max = Math.max(sizes[i][0], sizes[i][1]);
-        const min = Math.min(sizes[i][0], sizes[i][1]);
+    // for문을 돌며 각각의 최댓값과 최솟값을 찾음
+    // (가로 세로 상관없이 정렬하기 위함)
+    for(let s of sizes) {
+        let max = Math.max(s[0], s[1]);
+        let min = Math.min(s[0], s[1]);
         width.push(max);
         height.push(min);
     }
-
+    
+    // 가로와 세로 각 값에서 최댓값을 곱해 return
     return Math.max(...width) * Math.max(...height);
 }
