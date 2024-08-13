c3af945c972c3a91890ac9c7f9e87b249ef94433
추다율 <128597263+dayul@users.noreply.github.com>
2024-08-13 15:39:52 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.25 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,3 +1,6 @@
 def solution(arr, delete_list):
-    answer = list(filter(lambda a: a not in delete_list, arr))
+    # answer = list(filter(lambda a: a not in delete_list, arr))
+    
+    answer = [i for i in arr if i not in delete_list]
+    
     return answer
