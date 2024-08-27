bed005abf50553d0a6240ac0d8b6dd156c8a2286
추다율 <128597263+dayul@users.noreply.github.com>
2024-08-27 22:06:37 +0900

    [level 0] Title: 부분 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.py
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.py
@@ -1,11 +1,3 @@
 def solution(str1, str2):
-    answer = 0
-    
-    # 부분 문자열일 경우 2개이상으로 나눠짐
-    str = str2.split(str1)
-
-    # 2개 이상일 경우 1 저장
-    if len(str) > 1 :
-        answer = 1
-    
-    return answer
+    # true or false 값을 int()
+    return int(str1 in str2)
