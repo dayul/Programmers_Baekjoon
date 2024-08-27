49d6789b17d99b62d817b8d2477b713c204a1a11
추다율 <128597263+dayul@users.noreply.github.com>
2024-08-27 22:21:04 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,6 +1,5 @@
 def solution(num_list):
     evenSum = sum(num_list[::2])
     oddSum = sum(num_list[1::2])
-    
-    answer = evenSum if evenSum > oddSum else oddSum
-    return answer
+
+    return max(evenSum, oddSum)
