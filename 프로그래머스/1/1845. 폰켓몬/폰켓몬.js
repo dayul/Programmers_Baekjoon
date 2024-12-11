181f3ffc5cd9dd959d62637ce1d418cfa4bd5141
추다율 <128597263+dayul@users.noreply.github.com>
2024-12-12 00:27:02 +0900

    [level 1] Title: 폰켓몬, Time: 36.85 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,8 +1,9 @@
 function solution(nums) {
     var answer = 0;
-    var arr = [...new Set(nums)];
+    // var arr = [...new Set(nums)];
+    var arr = nums.filter((a, i) => nums.indexOf(a) === i);
     
-    answer = Math.min(nums.length / 2, arr.length)
+    answer = Math.min(nums.length / 2, arr.length);
 
     return answer;
 }
