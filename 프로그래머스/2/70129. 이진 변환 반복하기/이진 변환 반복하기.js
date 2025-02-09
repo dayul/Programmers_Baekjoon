36daf37f93af7926a542c6497087dabaff680e18
추다율 <128597263+dayul@users.noreply.github.com>
2025-02-09 23:41:45 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.07 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -7,8 +7,13 @@ function solution(s) {
     var answer = [0, 0];
     
     while(s !== '1') {
-        answer[1] += count_zero(s);     // 0의 개수 누적
-        s = s.replaceAll('0', '').length.toString(2);   // 0을 제외한 s의 길이를 이진변환
+        // 다른 분 방법 
+        // - 정규표현식을 사용해서 0의 개수를 구함 (null 방지)
+        answer[1] += (s.match(/0/g)||[]).length;
+        s = s.replace(/0/g, '').length.toString(2);
+        
+        // answer[1] += count_zero(s);     // 0의 개수 누적
+        // s = s.replaceAll('0', '').length.toString(2);   // 0을 제외한 s의 길이를 이진변환
         answer[0]++;    // 변환했으므로 횟수 증가
     }
     
