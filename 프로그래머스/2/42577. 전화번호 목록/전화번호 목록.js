3fe7abd72810d01b43a0c3b962b94162d95bd5b3
추다율 <dyulchoo@naver.com>
2025-02-26 15:58:28 +0900

    [level 2] Title: 전화번호 목록, Time: 156.91 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -0,0 +1,15 @@
+function solution(phone_book) {
+    var answer = true;
+    
+    // 뒤의 문자열만 비교할 수 있도록 먼저 정렬 (문자열 정렬)
+    phone_book.sort();
+    
+    for(let i = 0; i < phone_book.length - 1; i++) {
+        
+        // 앞 문자열이 뒤 문자열의 접두사인지 확인
+        if(phone_book[i + 1].startsWith(phone_book[i]))
+            answer = false;
+    }
+    
+    return answer;
+}
